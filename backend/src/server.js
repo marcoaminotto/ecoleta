c08c9a2b4b333c74d80fdac322ef9f67a38dcba5
Marco Echevestre <marcoaminotto@hotmail.com>
2020-07-18 12:46:00 +0200

    Install nunjuscks, configure it and create new routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const server = express();
 
 const frontendPath = "/Users/marcoechevestre/Documents/ecoletas/frontend/src";
 
-server.use(express.static(frontendPath));
-
-server.get("/", (req, res) => {
-  res.sendFile(frontendPath + "/pages/Main/index.html");
+nunjucks.configure(frontendPath + "/pages", {
+  express: server, 
+  noCache: true
 });
 
-server.listen(3000);
+server.use(express.static(frontendPath));
+
+
+server.get("/", (req, res) => {
+  return res.render("main.html");
+});
+ 
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+  return res.render("search-point.html");
+});
+
+server.listen(3100);
