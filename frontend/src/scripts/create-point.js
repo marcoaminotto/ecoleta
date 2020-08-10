4cf93dc64169b6a74477d462052203f32ff0a9c8
Marco Echevestre <marcoaminotto@hotmail.com>
2020-08-10 16:27:53 +0200

    Add the google map api at create-point page and show the localization of the selected city

--- frontend/src/scripts/create-point.js
+++ frontend/src/scripts/create-point.js
@@ -6,10 +6,21 @@ const countryOptions = document.querySelectorAll(
 );
 const regionSelect = document.querySelector("select[name=region]");
 const citySelect = document.querySelector("select[name=city]");
+const cityLongitude = document.querySelector("input[name=longitude]");
+const cityLatitude = document.querySelector("input[name=latitude]");
+const mapScreen = document.querySelector("#map");
 
 let selectedItems = [];
+let citiesCoordinates = [];
+let map;
+
+var locations = [
+  // here come a list of coordinates
+  // {lat: -31.563910, lng: 147.154312},
+];
 
 countrySelect.addEventListener("change", (event) => {
+  hideMap();
   regionSelect.innerHTML = '<option value="">Select a region</option>';
   citySelect.innerHTML = '<option value="">Select a city</option>';
   regionSelect.disabled = true;
@@ -30,6 +41,7 @@ countrySelect.addEventListener("change", (event) => {
 });
 
 regionSelect.addEventListener("change", (event) => {
+  hideMap();
   citySelect.innerHTML = '<option value="">Select a city</option>';
   citySelect.disabled = true;
 
@@ -43,11 +55,31 @@ regionSelect.addEventListener("change", (event) => {
     }).then((response) => {
       response.json().then((cities) => {
         for (const data of cities) {
+          citiesCoordinates.push({
+            city: data.city,
+            latitude: data.latitude,
+            longitude: data.longitude,
+          });
+          console.log(citiesCoordinates);
           citySelect.innerHTML += `<option value="${data.city}">${data.city}</option>`;
         }
       });
       citySelect.disabled = false;
     });
+  }
+});
+
+citySelect.addEventListener("change", (event) => {
+  if (event.target.value) {
+    const citySelected = citiesCoordinates.find(
+      (element) => element.city === event.target.value
+    );
+    cityLongitude.value = citySelected.longitude;
+    cityLatitude.value = citySelected.latitude;
+    initMap();
+    showMap();
+  } else {
+    hideMap();
   }
 });
 
@@ -82,4 +114,37 @@ function handleSelectedItem(event) {
   }
 
   collectedItems.value = selectedItems;
+}
+
+function initMap() {
+  let longitude = cityLongitude.value ? parseFloat(cityLongitude.value) : null;
+  let latitude = cityLatitude.value ? parseFloat(cityLatitude.value) : null;
+  console.log(longitude, latitude);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: latitude || -28.024, lng: longitude || 140.887 },
+    zoom: 12,
+  });
+
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+
+function hideMap() {
+  mapScreen.style.display = "none";
+}
+
+function showMap() {
+  mapScreen.style.display = "block";
 }
